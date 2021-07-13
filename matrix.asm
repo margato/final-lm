ac1ad280aa7a3009a4a25be37c07f383f3cc8c39
Osvaldo Margato <15236942+margato@users.noreply.github.com>
2021-07-12 21:35:34 -0300

    v1 multiplicaçao de matriz

--- matrix.asm
+++ matrix.asm
@@ -1,26 +1,6 @@
 SECTION .text
 global multiply_matrices_nasm
 
-; int **multiply_matrices(int **matrix_a, int **matrix_b, int **result, int n) {
-;     for (int i = 0; i < n; i++) {
-;         for (int j = 0; j < n; j++) {
-;             result[i][j] = 0;
-;             for (int k = 0; k < n; k++)
-;                 result[i][j] += matrix_a[i][k] * matrix_b[k][j];
-;         }
-;     }
-; }
-multiply_matrices_nasm:
-    push ebx
-    push ebp
-    mov ebp, esp            ; save esp
-
-    mov eax, [ebp + 12]     ; matrix_a address
-    mov ebx, [ebp + 16]     ; matrix_b address
-    mov ecx, [ebp + 20]     ; result address
-    mov edx, [ebp + 24]     ; n 
-    imul edx, edx           ; n = n^2             
-
 ; void multiply_matrices(int matrix_a[][n], int matrix_b[][n], int result[][n]) {
 ;     for (int i = 0; i < n; i++) {
 ;         for (int j = 0; j < n; j++) {
@@ -30,37 +10,83 @@ multiply_matrices_nasm:
 ;         }
 ;     }
 ; }
-    mov byte [ecx], 1
-    mov byte [ecx + 4], 2
-    mov byte [ecx + 8], 3
-    mov byte [ecx + 12], 4
 
-    push eax
+; calculates matrix offset and returns in eax
+; matrix_memory_offset = (row * n + column) * 4
+%macro get_offset 2
+    mov eax, %1                     ; eax = row
+    imul eax, n                     ; eax *= n
+    add eax, %2                     ; eax += column
+    imul eax, 4                     ; eax *= 4 bytes
+%endmacro
+
+
+; calculates matrix offset, grab matrix[x][y] value and returns in ebx
+%macro get_value_from_matrix 3
+    get_offset %1, %2
+    mov ecx, %3
+    mov ebx, [ecx + eax]            ; ebx = matrix[%1][%2], obtém valor da matrix + offset (eax)
+%endmacro
+
+; parâmetros
+%define matrix_a        dword [ebp + 12]
+%define matrix_b        dword [ebp + 16]
+%define result_matrix   dword [ebp + 20]
+%define n               dword [ebp + 24]
+
+; variáveis locais
+%define i               dword [ebp - 4]
+%define j               dword [ebp - 8]
+%define k               dword [ebp - 12]
+%define sum             dword [ebp - 16]
+%define aux             dword [ebp - 20]
+multiply_matrices_nasm:
     push ebx
+    push ebp
+    mov ebp, esp                                ; save esp
 
-    mov eax, 0                          ; i = 0
-    jmp loop_i_check
+    mov edx, n                                  ; n
+    mov i, 0                                    ; i = 0
+    loop_i:
+        mov ecx, i
+        cmp ecx, edx                            ; i >= n:
+        jge end_multiply_matrices_nasm          ;  goto end
+        mov j, 0                                ; j = 0   
+        loop_j:
+            mov ecx, j
+            cmp ecx, edx                                ; j >= n:
+            jge end_loop_j                              ;  goto loop i
+            mov sum, 0                                  ; sum = 0
+            mov k, 0                                    ; k = 0     
+            loop_k:
+                mov ecx, k
+                cmp ecx, edx                            ; k >= n:
+                jge end_loop_k                          ;  goto end_loop_k
 
-    loop_i:     
-        mov ebx, 0                      ; j = 0
-        jmp loop_j_check
-        loop_j:     
-            push eax                    ; save i
-            imul eax, 4                   
-            mov byte [ecx + eax], 0     ; result[i] = 1
-            pop eax                     ; restore i
-            inc ebx
-        loop_j_check:
-            cmp bx, dx                  ; i < n:
-            jl loop_j                   ;  goto loop
-        inc eax                         ; i++
-    loop_i_check:
-        cmp ax, dx                      ; i < n:
-        jl loop_i                       ;  goto loop
-    pop ebx
-    pop eax
+                get_value_from_matrix i, k, matrix_a    ; returns matrix_a[i][k] in ebx
+                mov aux, ebx                            ; push matrix_a[i][k]
 
-    mov esp, ebp
-    pop ebp
-    pop ebx
+                get_value_from_matrix k, j, matrix_b    ; returns matrix_a[k][j] in ebx
+                mov ecx, aux
+
+                imul ebx, ecx                         ; ebx *= ecx
+                add sum, ebx                            ; sum += ebx
+            
+                inc k                           ; k++
+                jmp loop_k
+
+                end_loop_k:
+                get_offset i, j
+                mov ecx, result_matrix
+                mov ebx, sum
+                mov [ecx + eax], ebx                  ; result[i][j] = sum
+                inc j                                 ; j++
+                jmp loop_j
+        end_loop_j:
+            inc i                               ; i++
+            jmp loop_i
+end_multiply_matrices_nasm:
+    mov esp, ebp                                ; restore esp
+    pop ebp      
+    pop ebx                               
     ret                             
